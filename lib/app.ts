bc47475a6d70f3d3dd72ac171f5e683beec3c4b5
bigyan.dahal@knovva.com <bigyan.dahal@knovva.com>
2018-03-26 16:16:47 -0400

    Changed method names.

--- lib/app.ts
+++ lib/app.ts
@@ -6,18 +6,18 @@ class App {
 
   constructor() {
     this.app = express();
-    this.config();
-    this.routes();
+    this.configureExpress();
+    this.mountRoutes();
   }
 
   public app: express.Application;
 
-  private config(): void {
+  private configureExpress(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
   }
 
-  private routes(): void {
+  private mountRoutes(): void {
     const router = express.Router();
 
     router.get('/', (req: Request, res: Response) => {
